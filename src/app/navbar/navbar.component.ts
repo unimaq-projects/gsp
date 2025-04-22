6806b1d6c052f0d980be2bd6db62387d25d83271
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-04-22 14:40:58 -0500

    feat: added select options in the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MultiSelectModule } from 'primeng/multiselect';
+import {ShowOnlyEnum} from '../request/enums/show-only.enum';
 
 @Component({
   selector: 'navbar-component',
-  imports: [],
+  imports: [MultiSelectModule],
   templateUrl: './navbar.component.html',
   standalone: true,
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  @Input() showOnly: string = "";
+  showOnlyOptions: { label: string; value: string }[] = [];
 
+  ngOnInit(): void {
+    this.showOnlyOptions = Object.values(ShowOnlyEnum).map((show) => ({label: show, value: show}));
+  }
 }
---
+++ src/app/request/enums/show-only.enum.ts
@@ -0,0 +1,7 @@
+export enum ShowOnlyEnum {
+    TODOS = "Todos",
+    PLANNER = "Planner",
+    PROGRAMADOR = "Programador",
+    ASISTENTE = "Asistente",
+    JEFE_REGIONAL = "Jefe Regional"
+}
